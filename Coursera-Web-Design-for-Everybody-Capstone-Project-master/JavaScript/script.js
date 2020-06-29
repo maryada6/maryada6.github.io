4f29fba3a9451b5f65901336467bbb7bcc1722f6
MARYADA KUMAR LODHA D <66838104+maryada6@users.noreply.github.com>
2020-06-29 20:19:58 +0530

    Add files via upload

---
+++ Coursera-Web-Design-for-Everybody-Capstone-Project-master/JavaScript/script.js
@@ -0,0 +1,21 @@
+
+// add function to mobile nav icons
+
+document.querySelector('.nav-icon').addEventListener('click', function()  {
+
+	document.querySelector('.menu').classList.add('opened');
+	document.querySelector('.menu').classList.remove('closed');
+
+	document.querySelector('.close').style.display = 'block';
+
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+
+	document.querySelector('.menu').classList.remove('opened');
+	document.querySelector('.menu').classList.add('closed');
+
+	document.querySelector('.close').style.display = 'none';
+
+
+});
